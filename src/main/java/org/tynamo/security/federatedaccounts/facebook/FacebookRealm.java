249f07f1d6d36e1634a29f03bef9ef9152ee1947
Alejandro Scandroli <ascandroli@codehaus.org>
2011-03-27 17:33:13 +0000

    removed some "// TODO Auto-generated constructor stub" messages

--- src/main/java/org/tynamo/security/federatedaccounts/facebook/FacebookRealm.java
+++ src/main/java/org/tynamo/security/federatedaccounts/facebook/FacebookRealm.java
@@ -112,97 +112,81 @@ public class FacebookRealm extends AuthenticatingRealm {
 	 */
 	@Override
 	public boolean isPermitted(PrincipalCollection principals, String permission) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isPermitted(PrincipalCollection subjectPrincipal, Permission permission) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean[] isPermitted(PrincipalCollection subjectPrincipal, String... permissions) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean[] isPermitted(PrincipalCollection subjectPrincipal, List<Permission> permissions) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean isPermittedAll(PrincipalCollection subjectPrincipal, String... permissions) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isPermittedAll(PrincipalCollection subjectPrincipal, Collection<Permission> permissions) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void checkPermission(PrincipalCollection subjectPrincipal, String permission) throws AuthorizationException {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void checkPermission(PrincipalCollection subjectPrincipal, Permission permission) throws AuthorizationException {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void checkPermissions(PrincipalCollection subjectPrincipal, String... permissions) throws AuthorizationException {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void checkPermissions(PrincipalCollection subjectPrincipal, Collection<Permission> permissions) throws AuthorizationException {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public boolean hasRole(PrincipalCollection subjectPrincipal, String roleIdentifier) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean[] hasRoles(PrincipalCollection subjectPrincipal, List<String> roleIdentifiers) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean hasAllRoles(PrincipalCollection subjectPrincipal, Collection<String> roleIdentifiers) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void checkRole(PrincipalCollection subjectPrincipal, String roleIdentifier) throws AuthorizationException {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void checkRoles(PrincipalCollection subjectPrincipal, Collection<String> roleIdentifiers) throws AuthorizationException {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void checkRoles(PrincipalCollection subjectPrincipal, String... roleIdentifiers) throws AuthorizationException {
-		// TODO Auto-generated method stub
 
 	}
 
